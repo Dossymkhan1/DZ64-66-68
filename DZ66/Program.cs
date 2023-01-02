0b7682203afaf046986cde7d9f67bac46506af4b
Your_name <dosss_xa@example.com>
2023-01-02 23:47:19 +0600

    added new file

---
+++ DZ66/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдет сумму элементов
+// в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+
+int SumElem(int m, int n)
+{
+if(n > m) return n > m ? n + SumElem(m, n - 1) : n;
+else return m > n ? m + SumElem(m - 1, n) : m;
+}
+Console.WriteLine(SumElem(4, 8));
